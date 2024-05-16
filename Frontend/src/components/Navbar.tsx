24f0b14492154b2c240f6837123af0749f589806
Rahul Kumar <rahulkhg980@gmail.com>
2024-05-16 13:32:20 +0530

    Added logo

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,25 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from '../assets/adiray_logo.png';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="bg-gray-800  justify-center items-center">
-      <div className="max-w-7xl h-[100px] mx-auto px-4 py-2 flex justify-between items-center">
-        <div className="flex items-center space-x-2">
-          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.urcfQ2YG9PS3mtS8cP38wQHaHa%26pid%3DApi&f=1&ipt=1adfcfd876e4db5e2c8e28ee704c958b9ccc72d9342ae95c135cc29325868110&ipo=images" alt="Logo" className="w-8 h-8" />
-          <Link to="https://adirayglobal.com/" className="text-white font-extrabold text-lg hover:text-gray-200 transition duration-300 cursor-pointer">
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 py-2 flex justify-between items-center">
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="w-16 h-16 rounded-sm" />
+          <Link to="https://adirayglobal.com/" className="text-white font-extrabold text-yellow-100 text-lg hover:text-yellow-400 transition duration-300 cursor-pointer">
             Adiray
           </Link>
         </div>
-        <div className="hidden md:block">
+        <div className="md:hidden flex items-center justify-center">
+          <button onClick={toggleNavbar} className="text-white focus:outline-none">
+            {isOpen ? (
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-100 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            )}
+          </button>
+        </div>
+        <div className={`${isOpen ? 'block' : 'hidden'} md:block `}>
           <div className="flex items-center gap-8">
-            <Link to="/" className="relative font-semibold text-white cursor-pointer text-[17px] w-fit md:block after:block after:content-['']
-    after:absolute after:h-[3px] after:bg-yellow-400 after:w-full after:scale-x-0 after:hover:scale-x-100 
-    after:transition after:duration-300 after:origin-center ">
+            <Link to="/" className="relative font-semibold text-white cursor-pointer text-[17px] w-fit md:block after:block after:content-[''] after:absolute after:h-[3px] after:bg-yellow-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center hover:text-yellow-400 text-yellow-100">
               Blog
             </Link>
-            <Link to="/" className="relative font-semibold text-white cursor-pointer text-[17px] w-fit md:block after:block after:content-['']
-    after:absolute after:h-[3px] after:bg-yellow-400 after:w-full after:scale-x-0 after:hover:scale-x-100 
-    after:transition after:duration-300 after:origin-center">
+            <Link to="/" className="relative font-semibold text-white cursor-pointer text-[17px] w-fit md:block after:block after:content-[''] after:absolute after:h-[3px] after:bg-yellow-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center text-yellow-100 hover:text-yellow-400">
               Products
             </Link>
           </div>
